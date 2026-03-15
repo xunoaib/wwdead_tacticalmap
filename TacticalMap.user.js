c6bef12fa72fcaa4361aa612dc3dbdef3e4077b9
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-03-15 08:20:54 -0400

    Remove download/update URLs

--- TacticalMap.user.js
+++ TacticalMap.user.js
@@ -7,15 +7,6 @@
 // @grant        GM.setValue
 // @grant        GM.getValue
 // @license      GNU General Public License v2 or later; http://www.gnu.org/licenses/gpl.txt
-
-// DTTL's Upstream Script:
-// // @downloadURL https://update.greasyfork.org/scripts/567867/WWDead%20Tactical%20Map%20v20.user.js
-// // @updateURL https://update.greasyfork.org/scripts/567867/WWDead%20Tactical%20Map%20v20.meta.js
-
-// My (xunoaib's) Fork:
-// @downloadURL https://raw.githubusercontent.com/xunoaib/wwdead_tacticalmap/main/TacticalMap.user.js
-// @updateURL   https://raw.githubusercontent.com/xunoaib/wwdead_tacticalmap/main/TacticalMap.meta.js
-
 // ==/UserScript==
 
 /**
