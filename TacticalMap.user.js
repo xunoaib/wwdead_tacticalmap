6c758896fe09d03f433e3461be524c4a41945ca2
xunoaib <13055788+xunoaib@users.noreply.github.com>
2026-03-15 23:46:35 -0400

    Fix: Make trailing forward slash in URL optional

--- TacticalMap.user.js
+++ TacticalMap.user.js
@@ -3,7 +3,7 @@
 // @namespace    wwd-mini-map-malton
 // @version      2.0.0-xun.1
 // @description  Displays city and suburb map in WWDead
-// @include /^https:\/\/wwdead\.com\/classic\/(\?.*)?$/
+// @include /^https:\/\/wwdead\.com\/classic\/?(\?.*)?$/
 // @grant        GM.setValue
 // @grant        GM.getValue
 // @license      GNU General Public License v2 or later; http://www.gnu.org/licenses/gpl.txt
